4ad8092f2922cc26d1af51a50fd100fcc39565ba
flycoderRuan <609387481@qq.com>
2020-02-27 19:30:38 +0800

    可视化数据

--- rzq_visualize.py
+++ rzq_visualize.py
@@ -92,8 +92,9 @@ def main(args=None):
 				cv2.rectangle(img, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
 				print(label_name)
 
-			cv2.imshow('img', img)
-			cv2.waitKey(0)
+			cv2.imwrite(os.path.join('/home/result_img',str(idx)+'.jpg'),img)
+			# cv2.imshow('img', img)
+			# cv2.waitKey(0)
 
 
 
